717aa81d3f47d5d9f8a57c7a34badb2c031d17ca
Pankaj <pankajkb469@gmail.com>
2023-10-08 23:16:32 +0530

    first concept of node server

--- Script.js
+++ Script.js
@@ -1,1 +1,30 @@
-console.log("first")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render("index", { Name: "pankaj Kumar" })
+});
+
+app.get('/message/:username', (req, res) => {
+    res.send(`Good Evening ${req.params.username}`)
+});
+
+app.use(function errorHandler(err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+}
+);
